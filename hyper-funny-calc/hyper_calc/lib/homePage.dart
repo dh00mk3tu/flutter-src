0ec469106df753e8b1fc790419322e7d0ff0468a
dh00mk3tu <cyberhybird@hotmail.com>
2021-04-17 20:03:26 +0530

    mega calc hyper horny

--- hyper-funny-calc/hyper_calc/lib/homePage.dart
+++ hyper-funny-calc/hyper_calc/lib/homePage.dart
@@ -6,6 +6,43 @@ class HomePage extends StatefulWidget{
 }
 
 class HomePageState extends State<HomePage> {
+
+  var n1=0, n2=0, res=0;
+  final TextEditingController t1 = new TextEditingController(text: "0");
+  final TextEditingController t2 = new TextEditingController(text: "0");
+
+  void callAdd() {
+    setState(() {
+      n1 = int.parse(t1.text);
+      n2 = int.parse(t2.text);
+      res = n1 + n2;
+
+    });
+  }
+  void callSub() {
+    setState(() {
+      n1 = int.parse(t1.text);
+      n2 = int.parse(t2.text);
+      res = n1 - n2;
+
+    });
+  }
+  void callPro() {
+    setState(() {
+      n1 = int.parse(t1.text);
+      n2 = int.parse(t2.text);
+      res = n1 * n2;
+
+    });
+  }
+  void callDiv() {
+    setState(() {
+      n1 = int.parse(t1.text);
+      n2 = int.parse(t2.text);
+      res = n1~/n2;
+
+    });
+  }
   @override 
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -14,10 +51,80 @@ class HomePageState extends State<HomePage> {
         title: Text("Hyper Mega Calc"),
       ),
       body: Container (
-        child: Center (
-         child: Column(
+        child: Padding(
+          padding: const EdgeInsets.all(40.0),
+          child: Column (
+            
+            mainAxisAlignment: MainAxisAlignment.center,
+            children : [
+              new Text(
+                "Result : $res",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.purple[900],
+                ),
+              ), 
+              TextField(
+                controller: t1,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  hintText: "Enter  Number"
+                ),
+              ),
+              TextField(
+                controller: t2,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  hintText: "Enter  Number"
+                ),
+              ),
+              new Padding(
+                padding: const EdgeInsets.only(
+                  top: 20.0
+                ),
+              ),
+              Row (
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                
+                children: [
+                  new MaterialButton(
+                    child: Text("+"),
+                    color: Colors.purple[300],
+                    onPressed: () => {
+                      callAdd()
+                    },
+                  ),
+                  new MaterialButton(
+                    child: Text("-"),
+                    color: Colors.purple[300],
+                    onPressed: () => {
+                      callSub()
+                    },
+                  ),
 
-         ),
+                ]
+              ),
+              Row (mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                   new MaterialButton(
+                    child: Text("x"),
+                    color: Colors.purple[300],
+                    onPressed: () => {
+                      callPro()
+                    },
+                  ),
+                  new MaterialButton(
+                    child: Text("/"),
+                    color: Colors.purple[300],
+                    onPressed: () => {
+                      callDiv()
+                    },
+                  ),
+                ],
+              )
+            ]
+          ),
         )
       ),
     );
