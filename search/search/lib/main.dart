3df74e6001a4ff58fa7e14db729e75e6c320c431
dh00mk3tu <cyberhybird@hotmail.com>
2021-04-19 13:29:22 +0530

    Credential Helper Test 2

--- search/search/lib/main.dart
+++ search/search/lib/main.dart
@@ -39,7 +39,7 @@ class HomePageState extends State<HomePage> {
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.stretch,
                         children: [
-                          Text("Name: "+mydata[index]['name']),
+                          // Text("Name: "+mydata[index]['name']),
                           Text("Age: "+mydata[index]['age']),
                           Text("Height: "+mydata[index]['height']),
                           Text("Gender: "+mydata[index]['gender']),
